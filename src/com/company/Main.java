b62c7e072ae85a697ac1aa890479bd7b69c0a584
Cofancs <cofancs@gmail.com>
2015-12-12 18:04:25 +0100

    I have made the correct answer for the second part too

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
         int totalWrapping=0;
+        int ribbonTotal=0;
 
         try {
             FileReader input= new FileReader("C:\\WorkSpace\\Advent\\Advent_day2\\src\\com\\company\\input.txt");
@@ -26,26 +27,38 @@ public class Main {
                 h= Integer.parseInt(seperated[2]);
 
                 int area=(2*l*w)+(2*l*h)+(2*h*w);
-                int min=l*w,temp=w*h,temp2=l*h;
+                int min=l*w,temp=w*h,temp2=l*h,minA=l,minB=w;
                 if(temp<min){
                     min=temp;
+                    minA=w;
+                    minB=h;
                     temp=temp2;
                     if(temp<min){
                         min=temp;
+                        minA=l;
+                        minB=h;
                     }
                 }else if (temp2<min){
                     min=temp2;
+                    minA=l;
+                    minB=h;
                     temp2=temp;
                     if(temp2<min){
                         min=temp;
+                        minA=w;
+                        minB=h;
                     }
                 }
+                int ribbonPlus=l*w*h;
+                int ribbonA=(2*minA)+(2*minB);
                 int total=area+min;
-                //System.out.println(l+" "+w+" "+h+" min: "+ min+" sqr: "+total );
+                System.out.println(l+" "+w+" "+h+" min: "+ min +" "+ minA +" "+minB+" sqr: "+total );
                 totalWrapping+=total;
+                ribbonTotal+=ribbonA+ribbonPlus;
                 line=bufferedReader.readLine();
             }
             System.out.println("total wrapping sqr : "+totalWrapping );
+            System.out.println("total ribbon wrapping sqr : "+ribbonTotal );
         } catch (IOException e) {
             e.printStackTrace();
         }
